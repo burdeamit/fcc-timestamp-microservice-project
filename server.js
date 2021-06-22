9ac8209b5503a773acf0a43089d487dd1a23c92e
Amit Kisan Burde <burdeamit@gmail.com>
2021-06-23 03:46:52 +0530

    date input changes

--- server.js
+++ server.js
@@ -2,74 +2,62 @@
 // where your node app starts
 
 // init project
-var express = require('express');
+var express = require("express");
 var app = express();
-require('dotenv').config()
-
+require("dotenv").config();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+// so that your API is remotely testable by FCC
+var cors = require("cors");
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+  res.sendFile(__dirname + "/views/index.html");
 });
 
-
-// your first API endpoint... 
+// your first API endpoint...
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  res.json({ greeting: "hello API" });
 });
 
-
-// API main endpoint 
-app.get('/api/', function (req, res) {
-  
-  let unixDate = new Date().getTime();
-  let utcDate = new Date().toUTCString();
+// API main endpoint
+app.get("/api/", function (req, res) {
   let dateApiObject = {
-    unix: unixDate,
-    utc: utcDate
-  }
-    res.json(dateApiObject);
+    unix: new Date().getTime(),
+    utc: new Date().toUTCString(),
+  };
+  res.json(dateApiObject);
 });
 
 // API endpoint for date parameters
-app.get('/api/:dateInput', function (req, res) {
+app.get("/api/:dateInput", function (req, res) {
   let dateInput = req.params.dateInput;
- 
+
   // Object Of Date API
   let dateApiObject = {
     unix: "",
-    utc: ""
-  }
- 
+    utc: "",
+  };
 
   // if date input contains "-" then
-  if (dateInput.includes('-')) {
-    console.log(req.params.dateInput);
-    // dateApiObject.unix = new Date(dateInput).getTime();
-    // dateApiObject.utc = new Date(dateInput).toGMTString();
-    let date_string = new Date(dateInput);
-    dateApiObject.unix = Date.parse(date_string);
-    dateApiObject.utc = date_string.toGMTString();
+  if (dateInput.includes("-")) {
+    dateApiObject.unix = new Date(dateInput).getTime();
+    dateApiObject.utc = new Date(dateInput).toUTCString();
   }
   // if date input does not contains "-"
   else {
-      // if date input contains non numeric characters 
-      if (dateInput.match(/[^0-9]/g)) {
-      dateApiObject.unix = 'Invalid Date';
-      dateApiObject.utc = 'Invalid Date';
-      }
-    
-     // if date input contains numeric characters only
-      else {
+    // if date input contains non numeric characters
+    if (dateInput.match(/[^0-9]/g)) {
+      dateApiObject.unix = "Invalid Date";
+      dateApiObject.utc = "Invalid Date";
+    }
 
+    // if date input contains numeric characters only
+    else {
       console.log(req.params.dateInput);
       dateInput = parseInt(dateInput);
       dateApiObject.unix = new Date(dateInput).getTime();
@@ -77,11 +65,14 @@ app.get('/api/:dateInput', function (req, res) {
     }
   }
   // if the dates are invalid show error
-  if (dateApiObject.unix === 'Invalid Date' || dateApiObject.utc === 'Invalid Date') {
+  if (
+    dateApiObject.unix === "Invalid Date" ||
+    dateApiObject.utc === "Invalid Date"
+  ) {
     res.json({ error: "Invalid Date" });
   }
-  
-  // if the dates are valid 
+
+  // if the dates are valid
   else {
     console.log(dateApiObject);
     res.json(dateApiObject);
@@ -90,5 +81,5 @@ app.get('/api/:dateInput', function (req, res) {
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+  console.log("Your app is listening on port " + listener.address().port);
 });
